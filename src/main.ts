bc8a1395877f1e87a855c5f8b12318b9f0bcd449
ptstp <animalfusion88@gmail.com>
2020-03-23 01:03:11 +0200

    next step

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log("!!!!!!!!!!Yo!!!!!!!!!")
+
+// const sleep = (milliseconds: number) => {
+//     return new Promise(resolve => setTimeout(resolve, milliseconds));
+// }
+
+
+
+
+// async function main() {
+    for (let x = 0; x < 10; x++) {
+        console.log("!!!!!!!!!!Yo!!!!!!!!!");
+        // await sleep(1000);
+    }
+// }
